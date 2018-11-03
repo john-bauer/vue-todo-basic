64667b098ac79c2ff098e9be1dbbbfa1beb4a75d
John Bauer <johnbauer126@gmail.com>
2018-11-03 14:16:05 -0400

    set up components for todo list and form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoList></TodoList>
+    <TodoForm></TodoForm>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import TodoForm from '@/components/TodoForm.vue';
+import TodoList from '@/components/TodoList.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    TodoList,
+    TodoForm,
   },
 };
 </script>
